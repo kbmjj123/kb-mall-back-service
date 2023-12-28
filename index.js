bbf3fdded956088144e5339b8c7a98bee502e9e4
郑耿林 <569751683@qq.com>
2023-12-28 10:29:20 +0800

    ✨ feat(统一响应结果): 统一响应处理结果
    1、提供自定义响应结果的中间件

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const notFoundMw = require('./middleware/not-found-middleware');
 const serviceErrorMw = require('./middleware/service-error-middleware');
+const responseWrapperMw = require('./middleware/response-wrapper-middleware.js');
 const routes = require('./router');
 const app = express();
 
 
+//! 追加响应体的中间件，统一格式化响应结果
+app.use(responseWrapperMw);
+
 app.get('/', (req, res) => {
-  res.json('服务成功访问了～～');
+  res.success('服务成功访问了～～');
 })
 routes(app);  // 借鉴于模块化管理，将路由对外暴露统一的一个接口
 
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const userRouter = express.Router()
 
-// 定义一用户路由拦截器中间件
+// 定义一"用户模块"路由拦截器中间件
 userRouter.use((req, res, next) => {
     console.log('userRouter Time:', Date.now())
     next()
 })
 
 // 通过id来获取用户信息
-userRouter.get('/', (req, res) => {
+userRouter.get('/', (req, res, next) => {
     res.json('我是来自于userRouter的json内容响应')
 })
 
