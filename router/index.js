76c6949c29b8f45f042ab8da70464b01db1c6f69
郑耿林 <kbmjj123@gmail.com>
2024-01-04 00:02:34 +0800

    ✨ feat(调整路由鉴权中间件位置): 通过公共模块与非公共模块的区分
    将需要经过鉴权后才能够访问的资源进行路由模块统一配置

--- router/index.js
+++ router/index.js
@@ -1,5 +1,7 @@
 const userRouter = require('./user-router');
+const commonRouter = require('./common-router');
 
 module.exports = app => {
+    app.use('/', commonRouter);
     app.use('/users', userRouter)
 }
