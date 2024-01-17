6366802cb50be8ccd9d688c1845e8a6a747d71bb
郑耿林 <kbmjj123@gmail.com>
2024-01-18 06:51:50 +0800

    ✨ feat(代码备份): 代码备份

---
+++ control/file-controller.js
@@ -0,0 +1,11 @@
+const asyncHandler = require('express-async-handler');
+
+module.exports = {
+  // 拿到文件后，对响应结果进行包装后返回
+  wrapFile: asyncHandler(async (req, res) => {
+    
+  }),
+  wrapFiles: asyncHandler(async (req, res) => {
+
+  })
+}
