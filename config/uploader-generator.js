ea26da2481cce55daa7f93295c59492fec3121cd
郑耿林 <kbmjj123@gmail.com>
2024-01-18 18:28:35 +0800

    ✨ feat(代码备份): 备份相关代码

--- config/uploader-generator.js
+++ config/uploader-generator.js
@@ -1,6 +1,16 @@
 const multer = require('multer');
 const BASE_FILE_DIR = 'uploads';
 const fs = require('fs');
+const crypto = require('crypto');
+const { readFile } = require('node:fs/promises');
+const { resolve } = require('node:path');
+
+// 计算缓冲区数据的哈希值的函数
+const calculateBufferHash = (buffer, algorithm = 'sha256') => {
+  const hash = crypto.createHash(algorithm);
+  hash.update(buffer);
+  return hash.digest('hex');
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -10,7 +20,7 @@ const storage = multer.diskStorage({
     }
     cb(null, `${BASE_FILE_DIR}/${req.query.path}/`)
   },
-  filename: function (req, file, cb) {
+  filename: async function (req, file, cb) {
     cb(null, file.originalname)
   }
 })
--- control/file-controller.js
+++ control/file-controller.js
@@ -3,6 +3,7 @@ const asyncHandler = require('express-async-handler');
 module.exports = {
   // 拿到文件后，对响应结果进行包装后返回
   wrapFile: asyncHandler(async (req, res) => {
+    console.info(req.file)
     res.success(req.file)
   }),
   wrapFiles: asyncHandler(async (req, res) => {
