3e7484121d4e45cbc7715d0b1ef90a5d72e3dc21
郑耿林 <kbmjj123@gmail.com>
2024-01-20 06:14:47 +0800

    ✨ feat(代码备份):

--- control/cate-controller.js
+++ control/cate-controller.js
@@ -5,7 +5,9 @@ module.exports = {
   // 获取分类列表数据
   getCateList: asyncHandler(async (req, res) => {}),
   // 新增分类
-  addACate: asyncHandler(async (req, res) => {}),
+  addACate: asyncHandler(async (req, res) => {
+    
+  }),
   // 修改分类
   editACate: asyncHandler(async (req, res) => {}),
   // 删除分类
