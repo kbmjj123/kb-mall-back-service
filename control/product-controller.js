13aca7a4730962b7a6ed84302d59e73a51987b0e
郑耿林 <kbmjj123@gmail.com>
2024-01-24 10:19:04 +0800

    ✨ feat(补充查询编辑商品功能): 补充查询编辑的相关动作

--- control/product-controller.js
+++ control/product-controller.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler');
 const productModel = require('../model/product-model');
 const brandModel = require('../model/brand-model');
 const cateModel = require('../model/cate-model');
+const { ObjectId } = require('mongodb');
 
 module.exports = {
   // 发布一个商品
@@ -43,7 +44,7 @@ module.exports = {
     const { id } = req.params;
     const params = req.body;
     if(id){
-      const updateAProduct = await productModel.findByIdAndUpdate(id, params);
+      const updateAProduct = await productModel.findByIdAndUpdate(id, params, {runValidators: true});
       console.info(updateAproduct);
       res.success(updateAProduct);
     }else{
@@ -71,14 +72,53 @@ module.exports = {
   }),
   // 删除一个商品
   removeAProduct: asyncHandler(async (req, res) => {
-
+    const { id } = req.params;
+    if(id){
+      const deleteAProduct = await productModel.deleteOne({ _id: new ObjectId(id) });
+      if(1 === deleteAProduct.deletedCount){
+        // 删除成功
+        res.success(id)
+      }else{
+        res.failed(-3, id, '操作失败，请稍后重试');
+      }
+    }else{
+      res.failed(-2, null, '请传递需要删除的商品id');
+    }
   }),
   // 获取一个商品明细信息
   getAProduct: asyncHandler(async (req, res) => {
-
+    const { id } = req.params;
+    if(id){
+      const findAProduct = await productModel.findOne({_id: new ObjectId(id)});
+      if(findAProduct){
+        res.success(findAProduct);
+      }else{
+        res.failed(-2, id, 'id错误，请检查后重试');
+      }
+    }else{
+      res.failed(-2, null, '请传递需要查询的商品id')
+    }
   }),
   // 分页获取商品列表信息
   getProductList: asyncHandler(async (req, res) => {
-    
+    let { keyword, pageIndex, pageSize } = req.body;
+    pageIndex -= 1;
+    let searchList = [], total = 0;
+    if(keyword){
+      const regex = new RegExp(keyword, 'i');
+      searchList = await productModel.find({ name: { $regex: regex } }).skip(pageIndex * pageSize).limit(pageSize);
+      total = await productModel.countDocuments({name: {$regex: regex}});
+    }else{
+      searchList = await productModel.find().skip(pageIndex * pageSize).limit(pageSize);
+      total = await productModel.estimatedDocumentCount();
+    }
+    let pages = Math.ceil(total / pageSize);
+    res.success({
+      list: searchList,
+      total,
+      pageSize: Number(pageSize),
+      pageIndex: Number(pageIndex + 1),
+      pages
+    })
   })
 };
