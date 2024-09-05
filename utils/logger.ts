06316a60bb593a41f8f9406055c6a67af8acdc1e
郑耿林 <kbmjj123@gmail.com>
2024-09-06 06:09:09 +0800

    🐞 fix(日志模块): 日志配置调整

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,7 +1,14 @@
 import Log4js from "log4js";
 Log4js.configure({
-	appenders: { cheese: { type: "file", filename: "cheese.log" } },
-	categories: { default: { appenders: ["cheese"], level: "error" } },
+	appenders: {
+    out: { type: 'console' },
+    app: { type: 'file', filename: 'application.log' },
+    errorFile: { type: 'file', filename: 'errors.log' }
+  },
+  categories: {
+    default: { appenders: ['out', 'app'], level: 'debug' },
+    error: { appenders: ['out', 'errorFile'], level: 'error' }
+  }
 })
 const Logger = Log4js.getLogger()
 Logger.level = 'debug'
