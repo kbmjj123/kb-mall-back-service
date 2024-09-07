17622af9562b687261667112fc75081c530bec71
郑耿林 <kbmjj123@gmail.com>
2024-09-07 09:38:58 +0800

    📃 docs(日志服务): 创建自定义的日志服务

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,9 +1,9 @@
 import Log4js from "log4js";
 Log4js.configure({
 	appenders: {
-    out: { type: 'console' },
-    app: { type: 'file', filename: 'logs/application.log' },
-    errorFile: { type: 'file', filename: 'logs/errors.log' }
+    out: { type: 'stdout' },
+    app: { type: 'dateFile', filename: 'logs/application.log', pattern: '.yyyy-MM-dd', keepFileExt: true },
+    errorFile: { type: 'dateFile', filename: 'logs/errors.log', pattern: '.yyyy-MM-dd', keepFileExt: true }
   },
   categories: {
     default: { appenders: ['out', 'app'], level: 'debug' },
